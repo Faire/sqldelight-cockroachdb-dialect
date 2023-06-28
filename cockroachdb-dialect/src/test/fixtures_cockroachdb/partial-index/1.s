806a7c30734002b1fb5d31bc680e1224eae941c6
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2023-06-28 12:05:46 -0400

    remove redundant brackets

--- cockroachdb-dialect/src/test/fixtures_cockroachdb/partial-index/1.s
+++ cockroachdb-dialect/src/test/fixtures_cockroachdb/partial-index/1.s
@@ -6,4 +6,4 @@ CREATE TABLE foo(
   INDEX idx_storing1 (bar1) WHERE (bar1 > 0)
 );
 
-CREATE INDEX idx_storing2 ON foo (bar2) WHERE (bar2 = TRUE);
+CREATE INDEX idx_storing2 ON foo (bar2) WHERE bar2 = TRUE;
