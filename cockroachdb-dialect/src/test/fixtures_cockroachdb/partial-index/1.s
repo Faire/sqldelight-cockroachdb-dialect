50955aab8ed0b6ac3287b879ad9a98464abb09f9
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2023-06-28 12:07:37 -0400

    Support Partial index on indices created in CREATE TABLE (#8)
    Support partial index in CREATE TABLE.
    
    CREATE INDEX already supported this.
    
    Reference:
    https://www.cockroachlabs.com/docs/v23.1/partial-indexes#creation

---
+++ cockroachdb-dialect/src/test/fixtures_cockroachdb/partial-index/1.s
@@ -0,0 +1,9 @@
+CREATE TABLE foo(
+  id INT NOT NULL,
+  bar1 INT NOT NULL,
+  bar2 BOOL NOT NULL,
+  PRIMARY KEY (id),
+  INDEX idx_storing1 (bar1) WHERE (bar1 > 0)
+);
+
+CREATE INDEX idx_storing2 ON foo (bar2) WHERE bar2 = TRUE;
