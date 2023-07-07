d31e1ae47df49eeed52f1c2c9b0dfc8ff301bcb5
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2023-07-07 17:28:19 -0400

    alter primary key integration test

--- integration-testing/src/test/kotlin/com/faire/sqldelight/dialects/cockroachdb/IntegrationTest.kt
+++ integration-testing/src/test/kotlin/com/faire/sqldelight/dialects/cockroachdb/IntegrationTest.kt
@@ -2,6 +2,7 @@ package com.faire.sqldelight.dialects.cockroachdb
 
 import app.cash.sqldelight.driver.jdbc.JdbcDriver
 import app.cash.sqldelight.driver.jdbc.asJdbcDriver
+import org.junit.AfterClass
 import org.junit.BeforeClass
 import org.junit.Test
 import org.postgresql.ds.PGSimpleDataSource
@@ -10,24 +11,36 @@ import org.testcontainers.containers.CockroachContainer
 class IntegrationTest {
   @Test
   fun `test`() {
-    CockroachDBIntegrationTesting.Schema.create(driver)
+    // empty for now
   }
 
   companion object {
-    lateinit var driver: JdbcDriver
+    private lateinit var container: CockroachContainer
+    private lateinit var driver: JdbcDriver
 
     @BeforeClass
     @JvmStatic
     fun beforeClass() {
-      val container = CockroachContainer("cockroachdb/cockroach")
+      container = CockroachContainer("cockroachdb/cockroach")
       container.start()
+
+      val cockroachdbPort = container.getMappedPort(26257)
 
       driver = PGSimpleDataSource().apply {
         applicationName = "sqldelight"
         databaseName = "postgres"
         user = "root"
-        portNumbers = listOf(container.firstMappedPort).toIntArray()
+        portNumbers = IntArray(1) { cockroachdbPort }
       }.asJdbcDriver()
+
+      CockroachDBIntegrationTesting.Schema.create(driver)
+    }
+
+    @AfterClass
+    @JvmStatic
+    fun afterClass() {
+      container.stop()
+      driver.close()
     }
   }
 }
