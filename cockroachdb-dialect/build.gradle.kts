cfba6a213ddc81eef7083105fb941b1767a4f709
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2023-07-10 14:36:34 -0400

    Revert "jitpack to build library (before going to maven) (#19)"
    This reverts commit 3b1855d578809c74abd54450128e3088e2c96580.

--- cockroachdb-dialect/build.gradle.kts
+++ cockroachdb-dialect/build.gradle.kts
@@ -2,17 +2,11 @@ plugins {
   alias(libs.plugins.grammar.kit.composer)
 }
 
-repositories {
-  maven("https://jitpack.io")
-}
-
 grammarKit {
   intellijRelease.set(libs.versions.idea)
 }
 
 dependencies {
-  implementation("com.github.Faire:sqldelight-cockroachdb-dialect:main-SNAPSHOT")
-
   compileOnly(libs.intellij.analysis)
   implementation(libs.intellij.util)
   api(libs.sqldelight.postgresql.dialect)
