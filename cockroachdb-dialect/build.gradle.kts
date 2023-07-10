ecc2b4dc987936e0765a7de7a26be37d57575189
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2023-07-10 14:07:32 -0400

    jitpack to build library (before going to maven)

--- cockroachdb-dialect/build.gradle.kts
+++ cockroachdb-dialect/build.gradle.kts
@@ -2,11 +2,17 @@ plugins {
   alias(libs.plugins.grammar.kit.composer)
 }
 
+repositories {
+  maven("https://jitpack.io")
+}
+
 grammarKit {
   intellijRelease.set(libs.versions.idea)
 }
 
 dependencies {
+  implementation("com.github.Faire:sqldelight-cockroachdb-dialect:main-SNAPSHOT")
+
   compileOnly(libs.intellij.analysis)
   implementation(libs.intellij.util)
   api(libs.sqldelight.postgresql.dialect)
