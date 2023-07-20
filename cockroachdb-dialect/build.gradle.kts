7e05887c4b4eabca32c3d1c8477b8d6764208237
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2023-07-20 15:59:58 -0400

    Publish to maven

--- cockroachdb-dialect/build.gradle.kts
+++ cockroachdb-dialect/build.gradle.kts
@@ -26,8 +26,32 @@ publishing {
     create<MavenPublication>("maven") {
       groupId = "com.faire"
       artifactId = "sqldelight-cockroachdb-dialect"
+      version = "0.1.0"
 
       from(components["java"])
+
+      pom {
+        name = "sqldelight-cockroachdb-dialect"
+        description = "A SQLDelight dialect for CockroachDB"
+        inceptionYear = "2023"
+
+        licenses {
+          license {
+            name = "The Apache License, Version 2.0"
+            url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+          }
+        }
+
+        issueManagement {
+          system = "GitHub"
+          url = "https://github.com/Faire/sqldelight-cockroachdb-dialect/issues"
+        }
+
+        scm {
+          connection = "scm:git:git://github.com/Faire/sqldelight-cockroachdb-dialect.git"
+          url = "https://github.com/Faire/sqldelight-cockroachdb-dialect"
+        }
+      }
     }
   }
 }
