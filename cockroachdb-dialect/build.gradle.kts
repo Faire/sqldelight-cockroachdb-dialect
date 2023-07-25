89129771ea449b2923fdd91eb9c37e0e4bc423c9
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2023-07-25 10:05:54 -0400

    Setup publishing to maven (#40)
    Already published a 0.1.0 release with this :)
    
    https://central.sonatype.com/artifact/com.faire/sqldelight-cockroachdb-dialect
    
    Documentation found here:
    https://vanniktech.github.io/gradle-maven-publish-plugin/central/
    
    Will be setting github actions to publish releases and snapshots

--- cockroachdb-dialect/build.gradle.kts
+++ cockroachdb-dialect/build.gradle.kts
@@ -1,6 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
+group = "com.faire"
+version = "0.1.1"
+
 plugins {
   alias(libs.plugins.grammar.kit.composer)
-  `maven-publish`
+  alias(libs.plugins.maven.publishing)
 }
 
 repositories {
@@ -21,13 +26,45 @@ dependencies {
   }
 }
 
-publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      groupId = "com.faire"
-      artifactId = "sqldelight-cockroachdb-dialect"
+mavenPublishing {
+  coordinates("com.faire", "sqldelight-cockroachdb-dialect", "0.1.1")
+  publishToMavenCentral(SonatypeHost.DEFAULT, true)
+  signAllPublications()
 
-      from(components["java"])
+  pom {
+    name = "sqldelight-cockroachdb-dialect"
+    description = "A SQLDelight dialect for CockroachDB"
+    inceptionYear = "2023"
+    url = "https://github.com/Faire/sqldelight-cockroachdb-dialect"
+
+    licenses {
+      license {
+        name = "The Apache License, Version 2.0"
+        distribution = "repo"
+        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+      }
+    }
+
+    developers {
+      developer {
+        id = "Faire"
+        name = "Faire Developers"
+      }
+    }
+
+    organization {
+      name = "Faire"
+      url = "https://www.faire.com"
+    }
+
+    issueManagement {
+      system = "GitHub"
+      url = "https://github.com/Faire/sqldelight-cockroachdb-dialect/issues"
+    }
+
+    scm {
+      connection = "scm:git:git://github.com/Faire/sqldelight-cockroachdb-dialect.git"
+      url = "https://github.com/Faire/sqldelight-cockroachdb-dialect"
     }
   }
 }
