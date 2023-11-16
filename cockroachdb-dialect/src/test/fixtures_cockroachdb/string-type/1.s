04d23c583060b45078ee8e4db88db5d638333f33
Bastien de Luca <de-luca@users.noreply.github.com>
2023-11-16 16:22:57 +0100

    Add support for STRING size (#59)
    Hi,
    
    This PR adds support for size parameter on the `STRING` keyword along
    side `CHAR` and `VARCHAR`

--- cockroachdb-dialect/src/test/fixtures_cockroachdb/string-type/1.s
+++ cockroachdb-dialect/src/test/fixtures_cockroachdb/string-type/1.s
@@ -1,10 +1,11 @@
 CREATE TABLE foo(
   id INT NOT NULL,
   name STRING NOT NULL,
+  code STRING(4) NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO foo(id, name) VALUES (1, 'bar');
+INSERT INTO foo(id, name, code) VALUES (1, 'bar', 'ABCD');
 
 SELECT *
 FROM foo
