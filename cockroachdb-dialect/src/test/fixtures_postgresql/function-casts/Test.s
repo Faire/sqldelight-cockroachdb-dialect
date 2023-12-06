03f706334a855fff8e61d8be520c733d6917b229
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2023-12-06 10:45:40 -0500

    Use PostgresqlTestFixtures instead of a copy

--- cockroachdb-dialect/src/test/fixtures_postgresql/function-casts/Test.s
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Posts(
-  date_created TEXT NOT NULL
-);
-
-SELECT count(*) FROM Posts
-WHERE age(CAST('epoch' AS TIMESTAMP) + date_created * CAST('1 millisecond' AS INTERVAL)) < CAST('1 week' AS INTERVAL);
