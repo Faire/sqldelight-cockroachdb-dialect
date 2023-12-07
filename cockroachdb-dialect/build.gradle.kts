2a8226df85cf907235f5c5914179b2be8eb92661
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2023-12-07 16:04:16 -0500

    prep for 0.3.0 release

--- cockroachdb-dialect/build.gradle.kts
+++ cockroachdb-dialect/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 group = "com.faire"
-version = "0.2.0"
+version = "0.3.0"
 
 plugins {
   alias(libs.plugins.grammar.kit.composer)
@@ -28,7 +28,7 @@ dependencies {
 }
 
 mavenPublishing {
-  coordinates("com.faire", "sqldelight-cockroachdb-dialect", "0.2.0")
+  coordinates("com.faire", "sqldelight-cockroachdb-dialect", "0.3.0")
   publishToMavenCentral(SonatypeHost.DEFAULT, true)
   signAllPublications()
 
